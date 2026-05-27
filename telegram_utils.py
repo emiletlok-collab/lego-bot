1d88525d884f747797b8769f7021c2c1c674f37d
TLOK Emile <emile@MacBook-Pro-de-TLOK.local>
2026-05-27 10:26:19 +0200

    fix railway telegram loop

--- telegram_utils.py
+++ telegram_utils.py
@@ -4,15 +4,20 @@ from config import TELEGRAM_TOKEN, CHAT_ID
 
 bot = Bot(token=TELEGRAM_TOKEN)
 
-def send_telegram_message(text):
-    async def send():
-        await bot.send_message(
-            chat_id=CHAT_ID,
-            text=text
-        )
+async def send_message_async(text):
+    await bot.send_message(
+        chat_id=CHAT_ID,
+        text=text
+    )
 
+def send_telegram_message(text):
     try:
-        asyncio.run(send())
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(send_message_async(text))
+        loop.close()
+
         print("✅ Message Telegram envoyé")
+
     except Exception as e:
         print("Erreur Telegram :", e)
